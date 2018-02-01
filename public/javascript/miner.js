9355e24af6a95bc2f1b227d0fa91a098a9143877
Martin Chapman <martin.chapman@kcl.ac.uk>
2018-02-01 18:07:37 +0000

    Computing hash.

--- public/javascript/miner.js
+++ public/javascript/miner.js
@@ -12,9 +12,37 @@ function httpGetAsync(theUrl, callback)
 
 function mine() {
 	
-	httpGetAsync("http://localhost:3001/miner/block", function(response) {
+	httpGetAsync("http://localhost:3001/miner/block", function(responseA) {
 		
-		console.log(response.nextBlock);
+		httpGetAsync("http://localhost:3001/blockchain/difficulty", function(responseB) {
+			
+			var hash;
+			
+			var block = responseA.nextBlock;
+			
+			var nonce = block.nonce;
+			
+			var md = forge.md.sha256.create();
+			
+			var hash;
+			
+			do {
+			
+				timestamp = new Date().getTime() / 1000;
+			    
+				nonce++;
+			    
+				md.update(block.index + block.previousHash + timestamp + JSON.stringify(block.transactions) + nonce);
+				
+				hash = md.digest().toHex();
+				
+				blockDifficulty = parseInt(hash.substring(0, 14), 16);
+				
+			} while ( blockDifficulty >= responseB.difficulty );
+			
+			console.log();
+			
+		})
 		
 	})
 
